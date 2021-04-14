074f58a733e6d8c3d3cd17395cfaea3fc6b52921
むずでょ <muzudho1@gmail.com>
2021-04-14 23:33:43 +0900

    position つくんのめんどくせ（＾～＾）嫌になった（＾～＾）

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
-import (
-	"github.com/muzudho/kifuwarabe-shogi2021/take1"
-)
+import "github.com/muzudho/kifuwarabe-shogi2021/take2"
 
 // main - 最初に実行されます
 func main() {
 	// fmt.Println("Hello, world!")
 
-	take1.MainLoop()
+	// take1.MainLoop()
+	take2.MainLoop()
 }
---
+++ take2/position.go
@@ -0,0 +1,12 @@
+package take2
+
+// Position - 局面
+type Position struct {
+	// Go言語で列挙型めんどくさいんで文字列で（＾～＾）
+	Board []string
+}
+
+// ReadPosition - 局面を読み取ります
+func ReadPosition(command string) {
+
+}
