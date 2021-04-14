211a5920352480a2da3b9dbadeb89c09ee3371d0
むずでょ <muzudho1@gmail.com>
2021-04-14 22:15:27 +0900

    入れ子フォルダーを修正

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/muzudho/kifuwarabe-shogi2021
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/muzudho/kifuwarabe-shogi2021/take1"
+)
+
+func main() { // 最初に実行されるmain()関数を定義
+	fmt.Println("main")
+
+	take1.Start()
+}
