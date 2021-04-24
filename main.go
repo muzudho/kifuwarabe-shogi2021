7ee2b68412b68749ced1b66dfc8a7365ab63f5bf
むずでょ <muzudho1@gmail.com>
2021-04-24 12:56:20 +0900

    take7

--- main.go
+++ main.go
@@ -4,19 +4,21 @@ import (
 	"math/rand"
 	"time"
 
-	"github.com/muzudho/kifuwarabe-shogi2021/take6"
+	"github.com/muzudho/kifuwarabe-shogi2021/take7"
 )
 
 // main - 最初に実行されます
 func main() {
-	// ゲーム向けの軽い乱数のタネ
-	rand.Seed(time.Now().UnixNano())
-
 	// fmt.Println("Hello, world!")
 	// take1.MainLoop()
 	// take2.MainLoop()
 	// take3.MainLoop()
 	// take4.MainLoop()
 	// take5.MainLoop()
-	take6.MainLoop()
+
+	// ゲーム向けの軽い乱数のタネ
+	rand.Seed(time.Now().UnixNano())
+
+	// take6.MainLoop()
+	take7.MainLoop()
 }
